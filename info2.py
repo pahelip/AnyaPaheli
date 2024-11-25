b06a557e78ec83dae6dee562daaef90a0a520140
pahelip <pahelipatel@gmail.com>
2024-11-25 15:23:41 -0500

    Add files via upload

---
+++ info2.py
@@ -0,0 +1,76 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "Images/Profilee.jpeg"
+about_me = "I'm Peter Parker. I'm a regular guy by day and Spider-Man by night! "
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/spongebob-squarepants-44b857281/"
+my_github_url = "https://github.com/spongebob-squarepants"
+my_email_address = "peterparker@gmail.com"
+
+
+education_data ={
+    'Degree': 'Graduate degree in biophysics and a doctorate in biochemistry',
+    'Institution': 'Empire State University',
+    'Location': 'New York, NY',
+    'Graduation Date': 'Never',
+    'GPA': '4.0'
+}
+course_data = {
+    "code":["PHYS 2211", "PHYS 2212", "CHEM 1310", "BIOS 1107"], 
+    "names":["Principles of Physics 1", "Principles of Physics 2", "General Chemistry", "Biological Principles"], 
+    "semester_taken":["2nd", "3rd", "2nd", "1st"],
+    "skills":["Nothing's falling faster than my grade", "No lightbulbs went off", "My Chemical Romance?", "Yet another science..."],
+    }
+experience_data = {
+    "Spider-Man": (["- Helped save the world multiple times",
+                                                                          "- Joined the Avengers"],"Images/saving.jpeg"),
+    "Photographer at the Daily Bugle":(["- Took pictures of Spiderman that no one else could ",
+                                                           "- Skilled in using cameras and understanding angles and lighting"],"Images/photographer.jpeg"),
+    "Scientist at Horizon Labs":(["- Analyzed complex problems and came up with creative solutions"],"Images/scientist.jpeg")
+
+}
+
+projects_data = {
+    "Web-Shooters": "Created wrist devices that shoot web fluid.",
+}
+
+programming_data = {
+    "Superhuman Strength": 60,
+    "Spider-Sense": 90,
+    "Adhesion": 40,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Superhuman Strength": "💪🏻",
+    "Spider-Sense": "🕸️",
+    "Adhesion": "🩹",
+}
+spoken_icons = {"French": "🇫🇷",
+    "English": "🇬🇧",
+    "Spanish":"🇪🇸"
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "French": "Conversational",
+    "Spanish": "Conversational",
+}
+leadership_data = {
+    "Leader of the Spider Army": (["- Led a group of Spider-People from across the multiverse to stop the Inheritors"],"Images/multiverse.jpeg"),
+
+}
+activity_data={
+    "The Parker Foundation": ["- Improved healthcare in impoverished communities", 
+            "- Funded research for curing diseases"]
+}
