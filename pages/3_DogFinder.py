75990287120ee51555e7572437538120204c9d88
Achaganti4 <anya.chaganti@gmail.com>
2024-11-26 04:27:35 +0000

    fixing dog data AGAIN

--- pages/3_DogFinder.py
+++ pages/3_DogFinder.py
@@ -4,7 +4,7 @@ API_URL = "https://api.thedogapi.com/v1/images/search"
 API_KEY = "live_bvXwtUCuseTj0LLHnlsUcvprBx0d0tn43fChCbza0AJ9DYnR4Xb6TYRuDRXKd4RE"
 
 def fetch_dog_data(limit = 1):
-    params = {"limit": 1,"api_key": API_KEY}
+    params = {"limit": limit,"api_key": API_KEY}
     response = requests.get(API_URL, params=params)
     return response.json()[0]
 
@@ -18,16 +18,24 @@ st.header("**Dog of the Day**")
 num = st.slider("Number of dogs:", min_value =  1, max_value = 10, value = 1)
 
 if "dog_data" not in st.session_state: #NEW
-    st.session_state["dog_data"] = fetch_dog_data()
+    st.session_state["dog_data"] = fetch_dog_data(limit = num)
 
-dog_data = st.session_state["dog_data"]
-st.image(dog_data["url"], caption="Dog of the Day")
+
 
 if st.button("Fetch New Dog"): #NEW
-    numDogs = fetch_dog_data(limit = num)
-    for dogType in numDogs:
-        st.image(dogType["url"])
-    st.session_state["dog_data"] = fetch_dog_data()
+    st.session_state["dog_data"] = fetch_dog_data(limit = num)
+
+dog_data = st.session_state["dog_data"]
+for dogType in dog_data:
+    st.image(dog_data["url"], caption="Dog of the Day")
+    if "breeds" in dogType and dogType["breeds"]:
+        st.write("Breed", dogType["breeds"[0]["name"])
+        st.write("Size", dogType["breeds"[0]["weight"]["imperial"])
+        st.write("Temperament", dogType["breeds"[0]["temperament"])
+    else:
+        st.write("No info on this breed.")
+        
+
 
 info_choice = st.radio("Select the information you want to see!", ["Breed", "Size", "Temperament"])
 
