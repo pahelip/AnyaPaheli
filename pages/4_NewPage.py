e7fb6308668322cfafc8e4412b65842aebc676b4
Achaganti4 <anya.chaganti@gmail.com>
2024-11-26 19:41:06 +0000

    import streamlit

--- pages/4_NewPage.py
+++ pages/4_NewPage.py
@@ -4,6 +4,7 @@ import streamlit as st
 
 key = st.secrets["key"]
 genai.configure(api_key = key)
+
 model = genai.GenerativeModel("gemini-1.5-flash") #this is the free model of google gemini
 response = model.generate_content("Write a poem about dogs!") #enter your prompt here!
 print(response.text) #dont forget to print your response!
