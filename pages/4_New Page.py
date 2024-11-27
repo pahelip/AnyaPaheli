7d75e70fd402e96cdfb236cab1d046f157b289e2
pahelip <pahelipatel@gmail.com>
2024-11-26 21:14:42 -0500

    Update 4_New Page.py

--- pages/4_New Page.py
+++ pages/4_New Page.py
@@ -5,67 +5,17 @@ import streamlit as st
 
 
 key = st.secrets["key"]
-genai.configure(api_key = key)
-
-"""
-breedData = {
-    "Bluetick Coonhound": {"size":"Medium"},
-    "African Hunting Dog": {"size":"Medium"}, 
-    "Australian Cattle Dog": {"size":"Medium"}, 
-    "Kooikerhondje": {"size":"Medium"}, 
-    "Saint Bernard": {"size":"Large"}, 
-    "Mastiff": {"size":"Large"}, 
-    "Bearded Collie": {"size":"Medium"}, 
-    "Irish Terrier": {"size":"Small"}, 
-    "Pomeranian": {"size":"Small"}, 
-    "Finnish Spitz": {"size":"Small"}
-
-}
-
-size = {"Small": 1, "Medium": 2, "Large":3}
-
-first = st.text_input("Enter your first dog breed:")
-second = st.text_input("Enter your second dog breed:")
-
-if st.button("Random dog"):
-    first,second = random.sample(list(breedData.keys()),2)
-    st.write(f"The randomly selected dogs are: {first}, {second}")
-if first and second:
-    if first in breedData and second in breedData:
-        one = breedData[first]
-        two = breedData[second]
-        sizeOne = size[one["size"]]
-        sizeTwo = size[two["size"]]
-        if sizeOne > sizeTwo:
-            winner = first
-        elif sizeTwo > sizeOne:
-            winner = second
-        else:
-            winner = None
-        st.write("Results:")
-        if winner:
-            st.write(f"{winner} is larger in size.")
-        else:
-            st.write(f"Both {first} and {second} are equal in size!")
-
-    else:
-        st.error("Results are not available for these breeds.")
-
-
-model = genai.GenerativeModel("gemini-1.5-flash") #this is the free model of google gemini
-response = model.generate_content("Write a story about these two dog breeds.") #enter your prompt here!
-print(response.text) #dont forget to print your response!
-"""
+genai.configure(api_key = key) 
 
 first = st.text_input("Enter your first dog breed:")
 second = st.text_input("Enter your second dog breed:")
 
 api_url="https://api.thedogapi.com/v1/breeds"
-api_key="live_bvXwtUCuseTj0LLHnlsUcvprBx0d0tn43fChCbza0AJ9DYnR4Xb6TYRuDRXKd4RE"
+API_key="live_bvXwtUCuseTj0LLHnlsUcvprBx0d0tn43fChCbza0AJ9DYnR4Xb6TYRuDRXKd4RE"
 
 def getDogData(dogBreed):
     try:
-        resp=requests.get(f"{api_url}?api_key={api_key}")
+        resp=requests.get(f"{api_url}?api_key={API_key}")
 
         breeds=resp.json()
 
